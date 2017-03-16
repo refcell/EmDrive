f588576c198db133834f9dfd922b0262ecc64400
Andreas Bigger <abigger87@gmail.com>
2017-03-15 22:28:06 -0400

    Update emdriveJavascript.js

--- emdriveJavascript.js
+++ emdriveJavascript.js
@@ -19,7 +19,7 @@ loader.load('EmDriveModel.dae', function ( collada )
     setMaterial(dae, new THREE.MeshBasicMaterial({color: 0xCC9933}));
     dae.scale.x = dae.scale.y = dae.scale.z = 5; 
     dae.updateMatrix();
-    dae.material.side = THREE.BackSide;
+    dae.doubleSided = true;
     init();
     animate();
 });

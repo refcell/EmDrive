5dcf1a07b9fb49269f447dbfc635c9250453f0f6
Andreas Bigger <abigger87@gmail.com>
2017-04-23 19:42:04 -0400

    Update emdriveJavascript.js

--- emdriveJavascript.js
+++ emdriveJavascript.js
@@ -110,7 +110,7 @@ function initMesh() {
 	if ( child instanceof THREE.Mesh ) {
 		console.log("instance");
 		child.geometry.computeFaceNormals();
-		child.material = new THREE.MeshBasicMaterial( { color: 0xCC9933, transparent: true, opacity: 0.5} );
+		child.material = new THREE.MeshBasicMaterial( { color: 0xCC9933} ); // , transparent: true, opacity: 0.5
 		child.material.side = THREE.DoubleSided;
 	}
 
